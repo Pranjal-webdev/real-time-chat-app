7b15ccf6522c39d099d56b0ddadddf2aceb64cbd
Pranjal <pranjalpundir37@gmail.com>
2026-09-15 18:16:45 +0530

    Allow production frontend in CORS

--- server/server.js
+++ server/server.js
@@ -27,6 +27,7 @@ app.use(
         origin: [
             "http://localhost:5173",
             "http://localhost:5174",
+            "https://real-time-chat-app-two-wine.vercel.app",
         ],
         credentials: true,
     })
--- server/socket/socket.js
+++ server/socket/socket.js
@@ -12,7 +12,8 @@ export const initializeSocket = (server) => {
         cors: {
             origin: [
                 "http://localhost:5173",
-                "http://localhost:5174"
+                "http://localhost:5174",
+                "https://real-time-chat-app-two-wine.vercel.app",
             ],
             credentials: true,
         },
